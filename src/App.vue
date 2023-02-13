3a107b483d03c215a857a6e4f17baed83f8d0b58
zjs <1824757520@qq.com>
2023-02-13 17:13:32 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+img{
+    width:100px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import 'amfe-flexible'
 createApp(App).mount('#app')
